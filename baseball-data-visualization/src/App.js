ae80c1645c6e45fabf079546477c31f8ccf09462
Raphael Palacio <54969177+raphaelpalacio@users.noreply.github.com>
2024-11-01 23:18:03 -0400

    wroking version

--- baseball-data-visualization/src/App.js
+++ baseball-data-visualization/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Dashboard from './components/Dashboard';
-import DetailPage from './components/DetailPage';
+import DetailPage from './pages/DetailPage';
 import Header from './components/Header';
 
 function App() {
