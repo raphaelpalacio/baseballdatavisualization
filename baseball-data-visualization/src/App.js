e37dbcbe50288118345938ab13e156c1ab307255
Raphael Palacio <54969177+raphaelpalacio@users.noreply.github.com>
2024-11-04 11:30:07 -0500

    adjusted comments in app.js

--- baseball-data-visualization/src/App.js
+++ baseball-data-visualization/src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import DetailPage from './pages/DetailPage';
 import Header from './components/Header';
-import HomePage from './pages/HomePage'; // Adjusted to import HomePage
+import HomePage from './pages/HomePage'; 
 
 function App() {
   return (
